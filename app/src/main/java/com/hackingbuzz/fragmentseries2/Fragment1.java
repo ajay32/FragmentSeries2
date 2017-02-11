91054a1af35fde2b9c50eac3cfddcbb829afafd6
ajay32 <ajay32mehta@gmail.com>
2017-02-11 02:53:48 -0800

    Initial commit

---
+++ app/src/main/java/com/hackingbuzz/fragmentseries2/Fragment1.java
@@ -0,0 +1,23 @@
+package com.hackingbuzz.fragmentseries2;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Avi Hacker on 8/18/2016.
+ */
+public class Fragment1 extends Fragment  {
+
+    Button b4;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag1,container,false);
+    }
+
+
+}
---
+++ app/src/main/java/com/hackingbuzz/fragmentseries2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.hackingbuzz.fragmentseries2;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+
+// always relplace parent of particular fragment
+//Actually a big myth...we dont relace activity at all....as much as we just replace fragment if ( we say transaction.replace) or we just add a fragment to another fragment if(transaction.add ) we do
+// and our main activity stays below always no replace ( as base for fragemnt)
+// 2 things done on fragemnt 1.) repace or add a fragemnt onto another (stack form )...if you add then you can go back to 1 fragment below by popback stack..
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+  public void a(View view) {
+      Fragment1 f1 = new Fragment1();
+      FragmentManager manager = getFragmentManager();
+      FragmentTransaction transaction = manager.beginTransaction();
+      transaction.replace(R.id.base_layout,f1,"frag1");   // here 1st parameter tell thell base layout onto we gonna put on our fragments..
+      transaction.commit();
+
+  }
+
+    public void b(View view) {
+        Fragment2 f2 = new Fragment2();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.base_layout,f2,"frag2");
+        transaction.commit();
+
+
+    }
+
+    public void c(View view) {
+        Fragment3 f3 = new Fragment3();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.base_layout,f3,"frag3");
+        transaction.commit();
+
+
+    }
+}
+
